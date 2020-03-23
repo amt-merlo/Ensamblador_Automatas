b355a3c5a7f72e1d7b8f2bee171b6b1180b06472
Fabrizio Ferreto <fabrizio.ferreto@gmail.com>
2020-03-23 16:56:14 -0600

    Tabla de simbolos

--- Pruebas.py
+++ Pruebas.py
@@ -1,3 +1,10 @@
 from automatas import *
 
-print(comando("adc R10,R15,abc"))
+var=['.program Primer ejemplo\n\n\n', '.const\ta = 28\n\tb = 0x2C3\n\n', '.text\n\n\n\tciclo_infinito:\n\t\tld R1,a(R0)\n\t\tld R2,b(R1)\n\t\tld R3,32(R2)\n\t\tadd R4,R3,R2\n\t\tand R4,R4,R2\n\tjmp ciclo_infinito\n\tst R4,a(R0)\t\n']
+
+for i in var:
+    print(i)
+print(programa(var))
+print("\n")
+print(tablaSimbolos)
+
