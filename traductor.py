d1bdf9a8666832b51cdc0c83f09d33409bbe7cb7
Allison M. Merlo <amt.merlo@gmail.com>
2020-03-28 12:38:26 -0600

    traductor v2

--- traductor.py
+++ traductor.py
@@ -1,6 +1,6 @@
 
 
-var = "add R4,R3,R2"
+var = "add R4,R3,a(R2)"
 
 
 
@@ -52,26 +52,45 @@ def regToBin(registro):
 
     return binFinal
 
+def ConstBin(registro):
+    x = 0
+    punt=0
+    while x < len(registro):
+        if registro[x] == "(":
+            break
+        punt+=1
+        x+=1
+    punt+=1
+    reg = registro[punt:len(registro)-1]
+    final = regToBin(reg)
+    return final
+
 
 def tipoA(codInstruccion, registros):
     #traduce el codigo de instruccion
     instruccion = instToBin(codInstruccion)
 
+    bandera = False #para saber si hay constante
+    registrosPosibles = ["R0","R1","R2","R3","R4","R5","R6","R7","R8","R9","R10","R11", "R12","R13","R14","R15"]
+
+
+    #pregunta si hay constante
+    if registros[2] not in registrosPosibles:
+
+        regFuente2 = ConstBin(registros[2])
+        tipo = '1' #asigna el tipo
+        K = '000000' #sacar valor con tabla de símbolos
+
+    else:
+        regFuente2 = regToBin(registros[2])
+        tipo = '0' #asigna el tipo
+        K = '000000' 
+
+
     #traduce los registros
     regDestino = regToBin(registros[0])
     regFuente1 = regToBin(registros[1])
-    regFuente2 = regToBin(registros[2])
-
-    #pregunta si hay constante
-    if registros[2] not in registros:
-        bandera = True
-
-    #asigna el tipo
-    if bandera:
-        tipo = '1'
-    else:
-        tipo = '0'
-        K = '000000' #tabla de simbolos
+    
 
     #pregunta por la condicion de testl
     if codInstruccion == 'testl':
@@ -90,14 +109,10 @@ def tipoA(codInstruccion, registros):
     final = funcion+' '+condicion+' '+tipo+' '+K+' '+regDestino+' '+regFuente1+' '+regFuente2+' '+instruccion 
     return final   
 
+
 def tipoI(codInstruccion, registros):
 
     instruccion = instToBin(codInstruccion)
-
-    x = 0
-    while x < len(registros[1]):
-        if 
-        x+=1
 
     regDato = regToBin(registros[0])
     regBase = regToBin(registros[1])
@@ -105,19 +120,13 @@ def tipoI(codInstruccion, registros):
     desplazamiento = '0000000000000000000'
 
 
-
     return ""
 
 def tipoJ(codInstruccion, registros):
-
-    
     return ""
 
-def traducir(var):
-    
-    bandera = False #para saber si hay constante
-    listaRegistros = ["R0","R1","R2","R3","R4","R5","R6","R7","R8","R9","R10","R11", "R12","R13","R14","R15"]
-
+#Traduce una sola instruccion
+def traducir(var): 
 
     #separa la instruccion de los registros
     lista = var.split(" ")
@@ -127,11 +136,12 @@ def traducir(var):
 
     #identifica tipo de instrucción por cantidad de registros
     if len(registros) == 3:
-        tipoA(lista[0], registros)
+        final = tipoA(lista[0], registros)
     elif len(registros) == 2:
-        tipoI(lista[0], registros)
+        final = tipoI(lista[0], registros)
     elif len(registros) == 1:
-        tipoJ(lista[0], registros)
+       final =  tipoJ(lista[0], registros)
+    return final
 
     
 
